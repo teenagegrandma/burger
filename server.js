8896c7245eb37d2466cc16765307f83b05153c25
teenagegrandma <rebecca.preston90@gmail.com>
2017-11-04 14:25:12 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,61 +1,24 @@
 //dependencies
 var express = require('express');
-var path = require('path');
-var methodOverride = require('method-override');
-
-//express app initialized
-var app = express();
-
-//setup
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-//config page favicon and public folder
-var favicon = require('serve-favicon');
-app.use(favicon(path.join(__dirname, '/public/img', 'Favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-//config body-parser 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
+var methodOverride = require('method-override')
+
+var app = express();
+//serve static content for the app from the "public directory in the applicaiton directory
+app.use(express.static(process.cwd() + '/public'));
+//app.use(express.static('public'));
+
+//parse application
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//console logger
-var logger = require('morgan');
-app.use(logger('dev'));
+//handlebars 
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-//config app routes
-app.use(methodOverride('_method'));
+var router = require('./controllers/burgers_controllers.js');
+app.use('/', router);
 
-var controller = require('./controllers/burgers_controllers');
-app.use('/', controller);
-
-//errors
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-//Error handler
-if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
-
-//error handler
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
-});
-
-module.exports = app;
+//open server
+var port = process.env.PORT || 3000;
+app.listen(port);
