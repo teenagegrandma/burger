4fd65fa2464335f61c4639244fe28acb2f6eda36
teenagegrandma <rebecca.preston90@gmail.com>
2017-11-30 20:58:49 -0600

    alter connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,15 +7,16 @@ if(process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
- 	// connection = mysql.createConnection("mysql://otkvq87rhzej5qx7:zan8ojyunakoip7f@tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mud2eihhpczqyukl");
- 	// var keys = require('./keys');
+ connection = mysql.createConnection("mysql://otkvq87rhzej5qx7:zan8ojyunakoip7f@tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mud2eihhpczqyukl");
+ //var keys = require('./keys');
 
  	connection = mysql.createConnection({
  	host: 'tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 	user: 'otkvq87rhzej5qx7',
 	password: 'zan8ojyunakoip7f', //do not know my password trying to figure it out :/
  	database: 'mud2eihhpczqyukl', //my database
-	port: 3306
+	port: 3306,
+	SSL: 'enabled with AES256-SHA'
  	});
 }
 
